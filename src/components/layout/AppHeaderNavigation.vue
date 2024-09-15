9993b52d27db7f3bdf2b4c67dd04ea4e13dccb7b
mayaif <margareta.ifrim@outlook.com>
2024-09-15 17:36:04 +0100

    Create AppHeaderNavigation.vue

---
+++ src/components/layout/AppHeaderNavigation.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+
+    <ul class="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
+        <li>
+            <RouterLink to="/" class="hover:underline">Home</RouterLink>
+        </li>
+        <li>
+            <RouterLink to="/todos" class="hover:underline">Todos</RouterLink>
+        </li>
+    </ul>
+
+</template>
