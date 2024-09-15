28475d126210fbee939c4fb8c2dc5ed13774d97b
mayaif <margareta.ifrim@outlook.com>
2024-09-15 17:36:47 +0100

    Create AppLayout.vue

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import AppHeader from './AppHeader.vue'
+</script>
+
+<template>
+    <div class="min-h-screen bg-background text-foreground">
+        <AppHeader />
+        <main>
+            <slot></slot>
+        </main>
+    </div>
+</template>
