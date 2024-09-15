077e7c2fc64058b2c6e26e6d184b0211014bbd2a
mayaif <margareta.ifrim@outlook.com>
2024-09-15 18:57:50 +0100

    Create TodosView.vue

---
+++ src/views/TodosView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TodoList from '@/components/todos/TodoList.vue'
+</script>
+
+<template>
+    <div class="container mx-auto px-2 py-8 mb-12">
+        <TodoList />
+    </div>
+</template>
